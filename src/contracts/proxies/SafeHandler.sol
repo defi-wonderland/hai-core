a3c55a89b9e138b2580e6c6dccde6675b1de92c7
Weißer Hase <wei3erHase@protonmail.com>
2023-05-26 12:29:22 +0200

    refactor: renamed safe handler

--- src/contracts/proxies/SafeHandler.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.6.7;
-
-import {SAFEEngine} from '../SAFEEngine.sol';
-
-contract SAFEHandler {
-  constructor(address safeEngine) public {
-    SAFEEngine(safeEngine).approveSAFEModification(msg.sender);
-  }
-}
