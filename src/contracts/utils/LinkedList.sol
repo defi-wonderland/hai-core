c0b1f4dcda1522f54352a34b86b3c8835e2c6453
0xRaccoon <raccoon@defi.sucks>
2023-05-30 18:02:57 -0300

    feat: add spike for LinkedList implementation
    Signed-off-by: 0xRaccoon <raccoon@defi.sucks>

---
+++ src/contracts/utils/LinkedList.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.19;
+
+import {ILinkedList} from '@interfaces/utils/ILinkedList.sol';
+
+contract LinkedList is ILinkedList {
+  // Valid keys will always start from 1, should never be confused with indexes which are the position in the list and are calculated
+  uint256 public head;
+  uint256 public size;
+
+  mapping(uint256 => Node) internal _nodes;
+
+  function nodes(uint256 _key) external view returns (Node memory _node) {
+    return _nodes[_key];
+  }
+
+  function push(address _contractAddress) external returns (uint256 _index) {
+    // The elements are added at the end of the list
+    Node memory _node = Node({
+      contractAddress: _contractAddress,
+      next: 0 // 0 means that there is no next element because it's the end of the list
+    });
+    ++size;
+    // adding element at the mapping
+    _nodes[size] = _node;
+    if (size == 0) {
+      // head should be the new node
+      head = size;
+    } else {
+      // the previous last element should point to the new node
+      _nodes[size - 1].next = size;
+    }
+
+    return size;
+  }
+
+  function remove(address _contractAddress) external returns (bool _success) {
+    if (size == 0) revert LinkedList_EmptyList();
+    uint256 _key = head;
+    uint256 _previousKey = 0;
+    while (_key != 0) {
+      if (_nodes[_key].contractAddress == _contractAddress) {
+        // The element to remove was found
+        if (_previousKey == 0) {
+          // reusable block between remove functions but letting it for clarity of the spike
+          // the element to remove is the head
+          head = _nodes[_key].next;
+        } else {
+          // the element to remove is not the head
+          _nodes[_previousKey].next = _nodes[_key].next;
+        }
+        delete _nodes[_key];
+        --size;
+        return true;
+      }
+      _previousKey = _key;
+      _key = _nodes[_key].next;
+    }
+    return false;
+  }
+
+  function remove(uint256 _index) external returns (bool _success, address _contractAddress) {
+    if (_index >= size) revert LinkedList_InvalidIndex(_index);
+    uint256 _key = head;
+    uint256 _previousKey = 0;
+    for (uint256 i = 0; i < _index; i++) {
+      if (i == _index) {
+        if (_previousKey == 0) {
+          // the element to remove is the head
+          head = _nodes[_key].next;
+        } else {
+          // the element to remove is not the head
+          _nodes[_previousKey].next = _nodes[_key].next;
+        }
+        _contractAddress = _nodes[_key].contractAddress;
+        delete _nodes[_key];
+        --size;
+        return (true, _contractAddress);
+      }
+      _previousKey = _key;
+      _key = _nodes[_key].next;
+    }
+    return (false, address(0));
+  }
+
+  function replace(address _contractAddress, uint256 _index) external returns (bool _success, address _removedAddress) {
+    if (_index >= size) revert LinkedList_InvalidIndex(_index);
+    uint256 _key = head;
+    for (uint256 i = 0; i < _index; i++) {
+      if (i == _index) {
+        _removedAddress = _nodes[_key].contractAddress;
+        _nodes[_key].contractAddress = _contractAddress;
+        return (true, _removedAddress);
+      }
+      _key = _nodes[_key].next;
+    }
+    return (false, address(0));
+  }
+
+  function replace(address _contractAddress, address _replacedAddress) external returns (bool _success, uint256 _index) {
+    if (size == 0) revert LinkedList_EmptyList();
+    uint256 _key = head;
+    uint256 _previousKey = 0;
+    while (_key != 0) {
+      if (_nodes[_key].contractAddress == _replacedAddress) {
+        _nodes[_key].contractAddress = _contractAddress;
+        return (true, _index);
+      }
+      _previousKey = _key;
+      _key = _nodes[_key].next;
+      ++_index;
+    }
+    return (false, 0);
+  }
+}
---
+++ src/interfaces/utils/ILinkedList.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.19;
+
+/**
+ * @title ILinkedList
+ * @dev This interface is used to implement a linked list data structure
+ * @dev goig to be implemented as FIFO, all the new items will be added at the end of the list, and the list will be iterated from the head to the end
+ */
+interface ILinkedList {
+  error LinkedList_EmptyList();
+  error LinkedList_InvalidIndex(uint256 _index);
+
+  struct Node {
+    address contractAddress; // Using address as the the value to store, but it could be bytes if we want to make it generic and store any type of data like we do with params
+    uint256 next; // The pointer to the next element in the list, 0 means that there is no next element, so it's the end of the list, dont confuse this with indexes, these are keys
+  }
+
+  /**
+   * @dev Returns the first element of the list
+   * @return _head The head of the list
+   */
+  function head() external view returns (uint256 _head);
+
+  /**
+   * @dev Returns the size of the list
+   * @return _size The size of the list
+   */
+  function size() external view returns (uint256 _size);
+
+  /**
+   * @dev Returns a node of the list given a key (this is not the index!)
+   * @param _key The key of the node to return
+   * @return _node The node of the list
+   */
+  function nodes(uint256 _key) external view returns (Node memory _node);
+  /**
+   * @dev Adds a new element to the end of the list
+   * @param _contractAddress The address of the contract to add
+   * @return _index The position in the list of the new element (it will be always the last one)
+   */
+  function push(address _contractAddress) external returns (uint256 _index);
+
+  /**
+   * @dev Removes an element from the list given the contract address
+   * @param _contractAddress The address of the contract to remove
+   * @return _success If the element was removed or not
+   */
+  function remove(address _contractAddress) external returns (bool _success);
+
+  /**
+   * @dev Removes an element from the list given the index (this is not the key! it's the position in the list)
+   * @param _index The index of the element to remove
+   * @return _success If the element was removed or not
+   * @return _contractAddress The address of the contract removed
+   */
+  function remove(uint256 _index) external returns (bool _success, address _contractAddress);
+
+  /**
+   * @dev Replaces an element from the list given the contract address, and an index position (this is not the key! it's the position in the list)
+   * @param _contractAddress The address of the contract to replace
+   * @param _index The index of the element to replace
+   * @return _success If the element was replaced or not
+   * @return _removedAddress The address of the contract removed
+   */
+  function replace(address _contractAddress, uint256 _index) external returns (bool _success, address _removedAddress);
+
+  /**
+   * @dev Replaces an element from the list given the contract address, and the address of the element to replace
+   * @param _contractAddress The address of the contract to replace
+   * @param _replacedAddress The address of the contract to replace
+   * @return _success If the element was replaced or not
+   * @return _index The index of the element replaced
+   */
+  function replace(address _contractAddress, address _replacedAddress) external returns (bool _success, uint256 _index);
+}
