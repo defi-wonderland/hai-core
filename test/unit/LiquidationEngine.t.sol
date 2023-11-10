a53deb40811773884275330d38412da331c3eebb
Weißer Hase <wei3erHase@protonmail.com>
2023-11-10 11:44:35 +0100

    feat: hardcoding fuzz seed

--- test/unit/LiquidationEngine.t.sol
+++ test/unit/LiquidationEngine.t.sol
@@ -767,6 +767,7 @@ contract Unit_LiquidationEngine_LiquidateSafe is Base {
     vm.assume(_liquidation.safeDebt > 0);
     vm.assume(_liquidation.safeCollateral > 0);
     vm.assume(_liquidation.liquidationPenalty > WAD);
+    vm.assume(_liquidation.liquidationQuantity > 0);
     vm.assume(_liquidation.accumulatedRate > 0);
     vm.assume(notOverflowMul(_liquidation.liquidationQuantity, WAD));
     vm.assume(notOverflowMul(_liquidation.safeCollateral, _liquidation.liquidationPrice));
