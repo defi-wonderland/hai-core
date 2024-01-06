7a7e1cd50317f4db4667677f3a1aada8e1a3be9b
Weißer Hase <wei3erHase@protonmail.com>
2024-01-06 18:15:24 -0300

    fix: separated token distributor deployment tests

--- test/e2e/Deploy.t.sol
+++ test/e2e/Deploy.t.sol
@@ -397,10 +397,12 @@ abstract contract CommonDeploymentTest is HaiTest, Deploy {
     assertEq(address(haiGovernor.timelock()), address(timelock));
   }
 
-  // TokenDistributor
-  function test_TokenDistributor() public {
+  function test_TokenDistributor_Bytecode() public {
     assertEq(address(tokenDistributor).code, type(TokenDistributor).runtimeCode);
+  }
 
+  // TokenDistributor
+  function test_TokenDistributor_Params() public {
     assertEq(protocolToken.balanceOf(address(tokenDistributor)), _tokenDistributorParams.totalClaimable);
     assertEq(protocolToken.totalSupply(), _tokenDistributorParams.totalClaimable);
 
