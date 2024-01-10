1bb4ae9a6f219e0b4eeabfdcc0ff2beacd777d5d
0xJabberwock <0xjabberwock@defi.sucks>
2024-01-10 01:28:47 -0300

    refactor: revert reordering in Common.s.sol

--- script/Common.s.sol
+++ script/Common.s.sol
@@ -11,148 +11,6 @@ abstract contract Common is Contracts, Params {
   uint256 internal _deployerPk = 69; // for tests
   uint256 internal _governorPK;
 
-  function deployTokens() public updateParams {
-    systemCoin = new SystemCoin('HAI Index Token', 'HAI');
-    protocolToken = new ProtocolToken('Protocol Token', 'KITE');
-  }
-
-  function deployGovernance() public updateParams {
-    IHaiGovernor.HaiGovernorParams memory _emptyGovernorParams;
-    // if governor params are not empty, deploy governor
-    if (keccak256(abi.encode(_governorParams)) != keccak256(abi.encode(_emptyGovernorParams))) {
-      haiGovernor = new HaiGovernor(protocolToken, 'HaiGovernor', _governorParams);
-
-      timelock = TimelockController(payable(haiGovernor.timelock()));
-
-      haiDelegatee = new HaiDelegatee(address(timelock));
-
-      // sets timelock as protocol governor
-      governor = address(timelock);
-    }
-  }
-
-  function deployContracts() public updateParams {
-    // deploy Base contracts
-    safeEngine = new SAFEEngine(_safeEngineParams);
-
-    oracleRelayer = new OracleRelayer(address(safeEngine), systemCoinOracle, _oracleRelayerParams);
-
-    surplusAuctionHouse =
-      new SurplusAuctionHouse(address(safeEngine), address(protocolToken), _surplusAuctionHouseParams);
-
-    debtAuctionHouse = new DebtAuctionHouse(address(safeEngine), address(protocolToken), _debtAuctionHouseParams);
-
-    accountingEngine = new AccountingEngine(
-      address(safeEngine), address(surplusAuctionHouse), address(debtAuctionHouse), _accountingEngineParams
-    );
-
-    liquidationEngine = new LiquidationEngine(address(safeEngine), address(accountingEngine), _liquidationEngineParams);
-
-    collateralAuctionHouseFactory =
-      new CollateralAuctionHouseFactory(address(safeEngine), address(liquidationEngine), address(oracleRelayer));
-
-    // deploy Token adapters
-    coinJoin = new CoinJoin(address(safeEngine), address(systemCoin));
-
-    collateralJoinFactory = new CollateralJoinFactory(address(safeEngine));
-  }
-
-  function deployTaxModule() public updateParams {
-    taxCollector = new TaxCollector(address(safeEngine), _taxCollectorParams);
-
-    stabilityFeeTreasury = new StabilityFeeTreasury(
-      address(safeEngine), address(accountingEngine), address(coinJoin), _stabilityFeeTreasuryParams
-    );
-  }
-
-  function _setupContracts() internal {
-    // auth
-    safeEngine.addAuthorization(address(oracleRelayer)); // modifyParameters
-    safeEngine.addAuthorization(address(coinJoin)); // transferInternalCoins
-    safeEngine.addAuthorization(address(taxCollector)); // updateAccumulatedRate
-    safeEngine.addAuthorization(address(debtAuctionHouse)); // transferInternalCoins [createUnbackedDebt]
-    safeEngine.addAuthorization(address(liquidationEngine)); // confiscateSAFECollateralAndDebt
-    surplusAuctionHouse.addAuthorization(address(accountingEngine)); // startAuction
-    debtAuctionHouse.addAuthorization(address(accountingEngine)); // startAuction
-    accountingEngine.addAuthorization(address(liquidationEngine)); // pushDebtToQueue
-    protocolToken.addAuthorization(address(debtAuctionHouse)); // mint
-    systemCoin.addAuthorization(address(coinJoin)); // mint
-
-    safeEngine.addAuthorization(address(collateralJoinFactory)); // addAuthorization(cJoin child)
-  }
-
-  function deployGlobalSettlement() public updateParams {
-    globalSettlement = new GlobalSettlement(
-      address(safeEngine),
-      address(liquidationEngine),
-      address(oracleRelayer),
-      address(coinJoin),
-      address(collateralJoinFactory),
-      address(collateralAuctionHouseFactory),
-      address(stabilityFeeTreasury),
-      address(accountingEngine),
-      _globalSettlementParams
-    );
-
-    postSettlementSurplusAuctionHouse =
-      new PostSettlementSurplusAuctionHouse(address(safeEngine), address(protocolToken), _postSettlementSAHParams);
-
-    settlementSurplusAuctioneer =
-      new SettlementSurplusAuctioneer(address(accountingEngine), address(postSettlementSurplusAuctionHouse));
-  }
-
-  function _setupGlobalSettlement() internal {
-    // setup globalSettlement [auth: disableContract]
-    safeEngine.addAuthorization(address(globalSettlement));
-    liquidationEngine.addAuthorization(address(globalSettlement));
-    stabilityFeeTreasury.addAuthorization(address(globalSettlement));
-    accountingEngine.addAuthorization(address(globalSettlement));
-    oracleRelayer.addAuthorization(address(globalSettlement));
-    coinJoin.addAuthorization(address(globalSettlement));
-    collateralJoinFactory.addAuthorization(address(globalSettlement));
-    collateralAuctionHouseFactory.addAuthorization(address(globalSettlement)); // [+ terminateAuctionPrematurely]
-
-    // registry
-    accountingEngine.modifyParameters('postSettlementSurplusDrain', abi.encode(settlementSurplusAuctioneer));
-
-    // auth
-    postSettlementSurplusAuctionHouse.addAuthorization(address(settlementSurplusAuctioneer)); // startAuction
-  }
-
-  function deployPIDController() public updateParams {
-    pidController = new PIDController({
-      _cGains: _pidControllerGains,
-      _pidParams: _pidControllerParams,
-      _importedState: IPIDController.DeviationObservation(0, 0, 0)
-    });
-
-    pidRateSetter = new PIDRateSetter({
-      _oracleRelayer: address(oracleRelayer),
-      _pidCalculator: address(pidController),
-      _pidRateSetterParams: _pidRateSetterParams
-    });
-  }
-
-  function _setupPIDController() internal {
-    // setup registry
-    pidController.modifyParameters('seedProposer', abi.encode(pidRateSetter));
-
-    // auth
-    oracleRelayer.addAuthorization(address(pidRateSetter));
-  }
-
-  function deployJobContracts() public updateParams {
-    accountingJob = new AccountingJob(address(accountingEngine), address(stabilityFeeTreasury), JOB_REWARD);
-    liquidationJob = new LiquidationJob(address(liquidationEngine), address(stabilityFeeTreasury), JOB_REWARD);
-    oracleJob = new OracleJob(address(oracleRelayer), address(pidRateSetter), address(stabilityFeeTreasury), JOB_REWARD);
-  }
-
-  function _setupJobContracts() internal {
-    stabilityFeeTreasury.setTotalAllowance(address(accountingJob), type(uint256).max);
-    stabilityFeeTreasury.setTotalAllowance(address(liquidationJob), type(uint256).max);
-    stabilityFeeTreasury.setTotalAllowance(address(oracleJob), type(uint256).max);
-  }
-
   function deployCollateralContracts(bytes32 _cType) public updateParams {
     // deploy CollateralJoin and CollateralAuctionHouse
     address _delegatee = delegatee[_cType];
@@ -170,48 +28,6 @@ abstract contract Common is Contracts, Params {
     collateralAuctionHouseFactory.initializeCollateralType(_cType, abi.encode(_collateralAuctionHouseParams[_cType]));
     collateralAuctionHouse[_cType] =
       ICollateralAuctionHouse(collateralAuctionHouseFactory.collateralAuctionHouses(_cType));
-  }
-
-  function _setupCollateral(bytes32 _cType) internal {
-    safeEngine.initializeCollateralType(_cType, abi.encode(_safeEngineCParams[_cType]));
-    oracleRelayer.initializeCollateralType(_cType, abi.encode(_oracleRelayerCParams[_cType]));
-    liquidationEngine.initializeCollateralType(_cType, abi.encode(_liquidationEngineCParams[_cType]));
-
-    taxCollector.initializeCollateralType(_cType, abi.encode(_taxCollectorCParams[_cType]));
-    if (_taxCollectorSecondaryTaxReceiver.receiver != address(0)) {
-      taxCollector.modifyParameters(_cType, 'secondaryTaxReceiver', abi.encode(_taxCollectorSecondaryTaxReceiver));
-    }
-
-    // setup initial price
-    oracleRelayer.updateCollateralPrice(_cType);
-  }
-
-  function deployProxyContracts(address _safeEngine) public updateParams {
-    proxyFactory = new HaiProxyFactory();
-    safeManager = new HaiSafeManager(_safeEngine);
-    _deployProxyActions();
-  }
-
-  function _deployProxyActions() internal {
-    basicActions = new BasicActions();
-    debtBidActions = new DebtBidActions();
-    surplusBidActions = new SurplusBidActions();
-    collateralBidActions = new CollateralBidActions();
-    postSettlementSurplusBidActions = new PostSettlementSurplusBidActions();
-    globalSettlementActions = new GlobalSettlementActions();
-    rewardedActions = new RewardedActions();
-  }
-
-  function deployTokenDistributor() public updateParams {
-    ITokenDistributor.TokenDistributorParams memory _emptyTokenDistributorParams;
-    // if token distributor params are not empty, deploy token distributor
-    if (keccak256(abi.encode(_tokenDistributorParams)) != keccak256(abi.encode(_emptyTokenDistributorParams))) {
-      // Deploy aidrop distributor contract
-      tokenDistributor = new TokenDistributor(address(protocolToken), _tokenDistributorParams);
-
-      // auth
-      protocolToken.addAuthorization(address(tokenDistributor)); // mint
-    }
   }
 
   function _revokeAllTo(address _governor) internal {
@@ -324,6 +140,190 @@ abstract contract Common is Contracts, Params {
 
   function _shouldRevoke() internal view returns (bool) {
     return governor != deployer && governor != address(0);
+  }
+
+  function deployTokens() public updateParams {
+    systemCoin = new SystemCoin('HAI Index Token', 'HAI');
+    protocolToken = new ProtocolToken('Protocol Token', 'KITE');
+  }
+
+  function deployGovernance() public updateParams {
+    IHaiGovernor.HaiGovernorParams memory _emptyGovernorParams;
+    // if governor params are not empty, deploy governor
+    if (keccak256(abi.encode(_governorParams)) != keccak256(abi.encode(_emptyGovernorParams))) {
+      haiGovernor = new HaiGovernor(protocolToken, 'HaiGovernor', _governorParams);
+
+      timelock = TimelockController(payable(haiGovernor.timelock()));
+
+      haiDelegatee = new HaiDelegatee(address(timelock));
+
+      // sets timelock as protocol governor
+      governor = address(timelock);
+    }
+  }
+
+  function deployContracts() public updateParams {
+    // deploy Base contracts
+    safeEngine = new SAFEEngine(_safeEngineParams);
+
+    oracleRelayer = new OracleRelayer(address(safeEngine), systemCoinOracle, _oracleRelayerParams);
+
+    surplusAuctionHouse =
+      new SurplusAuctionHouse(address(safeEngine), address(protocolToken), _surplusAuctionHouseParams);
+
+    debtAuctionHouse = new DebtAuctionHouse(address(safeEngine), address(protocolToken), _debtAuctionHouseParams);
+
+    accountingEngine = new AccountingEngine(
+      address(safeEngine), address(surplusAuctionHouse), address(debtAuctionHouse), _accountingEngineParams
+    );
+
+    liquidationEngine = new LiquidationEngine(address(safeEngine), address(accountingEngine), _liquidationEngineParams);
+
+    collateralAuctionHouseFactory =
+      new CollateralAuctionHouseFactory(address(safeEngine), address(liquidationEngine), address(oracleRelayer));
+
+    // deploy Token adapters
+    coinJoin = new CoinJoin(address(safeEngine), address(systemCoin));
+
+    collateralJoinFactory = new CollateralJoinFactory(address(safeEngine));
+  }
+
+  function deployTaxModule() public updateParams {
+    taxCollector = new TaxCollector(address(safeEngine), _taxCollectorParams);
+
+    stabilityFeeTreasury = new StabilityFeeTreasury(
+      address(safeEngine), address(accountingEngine), address(coinJoin), _stabilityFeeTreasuryParams
+    );
+  }
+
+  function deployGlobalSettlement() public updateParams {
+    globalSettlement = new GlobalSettlement(
+      address(safeEngine),
+      address(liquidationEngine),
+      address(oracleRelayer),
+      address(coinJoin),
+      address(collateralJoinFactory),
+      address(collateralAuctionHouseFactory),
+      address(stabilityFeeTreasury),
+      address(accountingEngine),
+      _globalSettlementParams
+    );
+
+    postSettlementSurplusAuctionHouse =
+      new PostSettlementSurplusAuctionHouse(address(safeEngine), address(protocolToken), _postSettlementSAHParams);
+
+    settlementSurplusAuctioneer =
+      new SettlementSurplusAuctioneer(address(accountingEngine), address(postSettlementSurplusAuctionHouse));
+  }
+
+  function deployTokenDistributor() public updateParams {
+    ITokenDistributor.TokenDistributorParams memory _emptyTokenDistributorParams;
+    // if token distributor params are not empty, deploy token distributor
+    if (keccak256(abi.encode(_tokenDistributorParams)) != keccak256(abi.encode(_emptyTokenDistributorParams))) {
+      // Deploy aidrop distributor contract
+      tokenDistributor = new TokenDistributor(address(protocolToken), _tokenDistributorParams);
+
+      // auth
+      protocolToken.addAuthorization(address(tokenDistributor)); // mint
+    }
+  }
+
+  function _setupGlobalSettlement() internal {
+    // setup globalSettlement [auth: disableContract]
+    safeEngine.addAuthorization(address(globalSettlement));
+    liquidationEngine.addAuthorization(address(globalSettlement));
+    stabilityFeeTreasury.addAuthorization(address(globalSettlement));
+    accountingEngine.addAuthorization(address(globalSettlement));
+    oracleRelayer.addAuthorization(address(globalSettlement));
+    coinJoin.addAuthorization(address(globalSettlement));
+    collateralJoinFactory.addAuthorization(address(globalSettlement));
+    collateralAuctionHouseFactory.addAuthorization(address(globalSettlement)); // [+ terminateAuctionPrematurely]
+
+    // registry
+    accountingEngine.modifyParameters('postSettlementSurplusDrain', abi.encode(settlementSurplusAuctioneer));
+
+    // auth
+    postSettlementSurplusAuctionHouse.addAuthorization(address(settlementSurplusAuctioneer)); // startAuction
+  }
+
+  function _setupContracts() internal {
+    // auth
+    safeEngine.addAuthorization(address(oracleRelayer)); // modifyParameters
+    safeEngine.addAuthorization(address(coinJoin)); // transferInternalCoins
+    safeEngine.addAuthorization(address(taxCollector)); // updateAccumulatedRate
+    safeEngine.addAuthorization(address(debtAuctionHouse)); // transferInternalCoins [createUnbackedDebt]
+    safeEngine.addAuthorization(address(liquidationEngine)); // confiscateSAFECollateralAndDebt
+    surplusAuctionHouse.addAuthorization(address(accountingEngine)); // startAuction
+    debtAuctionHouse.addAuthorization(address(accountingEngine)); // startAuction
+    accountingEngine.addAuthorization(address(liquidationEngine)); // pushDebtToQueue
+    protocolToken.addAuthorization(address(debtAuctionHouse)); // mint
+    systemCoin.addAuthorization(address(coinJoin)); // mint
+
+    safeEngine.addAuthorization(address(collateralJoinFactory)); // addAuthorization(cJoin child)
+  }
+
+  function _setupCollateral(bytes32 _cType) internal {
+    safeEngine.initializeCollateralType(_cType, abi.encode(_safeEngineCParams[_cType]));
+    oracleRelayer.initializeCollateralType(_cType, abi.encode(_oracleRelayerCParams[_cType]));
+    liquidationEngine.initializeCollateralType(_cType, abi.encode(_liquidationEngineCParams[_cType]));
+
+    taxCollector.initializeCollateralType(_cType, abi.encode(_taxCollectorCParams[_cType]));
+    if (_taxCollectorSecondaryTaxReceiver.receiver != address(0)) {
+      taxCollector.modifyParameters(_cType, 'secondaryTaxReceiver', abi.encode(_taxCollectorSecondaryTaxReceiver));
+    }
+
+    // setup initial price
+    oracleRelayer.updateCollateralPrice(_cType);
+  }
+
+  function deployPIDController() public updateParams {
+    pidController = new PIDController({
+      _cGains: _pidControllerGains,
+      _pidParams: _pidControllerParams,
+      _importedState: IPIDController.DeviationObservation(0, 0, 0)
+    });
+
+    pidRateSetter = new PIDRateSetter({
+      _oracleRelayer: address(oracleRelayer),
+      _pidCalculator: address(pidController),
+      _pidRateSetterParams: _pidRateSetterParams
+    });
+  }
+
+  function _setupPIDController() internal {
+    // setup registry
+    pidController.modifyParameters('seedProposer', abi.encode(pidRateSetter));
+
+    // auth
+    oracleRelayer.addAuthorization(address(pidRateSetter));
+  }
+
+  function deployJobContracts() public updateParams {
+    accountingJob = new AccountingJob(address(accountingEngine), address(stabilityFeeTreasury), JOB_REWARD);
+    liquidationJob = new LiquidationJob(address(liquidationEngine), address(stabilityFeeTreasury), JOB_REWARD);
+    oracleJob = new OracleJob(address(oracleRelayer), address(pidRateSetter), address(stabilityFeeTreasury), JOB_REWARD);
+  }
+
+  function _setupJobContracts() internal {
+    stabilityFeeTreasury.setTotalAllowance(address(accountingJob), type(uint256).max);
+    stabilityFeeTreasury.setTotalAllowance(address(liquidationJob), type(uint256).max);
+    stabilityFeeTreasury.setTotalAllowance(address(oracleJob), type(uint256).max);
+  }
+
+  function deployProxyContracts(address _safeEngine) public updateParams {
+    proxyFactory = new HaiProxyFactory();
+    safeManager = new HaiSafeManager(_safeEngine);
+    _deployProxyActions();
+  }
+
+  function _deployProxyActions() internal {
+    basicActions = new BasicActions();
+    debtBidActions = new DebtBidActions();
+    surplusBidActions = new SurplusBidActions();
+    collateralBidActions = new CollateralBidActions();
+    postSettlementSurplusBidActions = new PostSettlementSurplusBidActions();
+    globalSettlementActions = new GlobalSettlementActions();
+    rewardedActions = new RewardedActions();
   }
 
   function _deployUniV3Pool(
